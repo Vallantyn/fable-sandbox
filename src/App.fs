60a9240c6496ff7b5cc0034db11e6bdf41aff104
Charlie Murtin <cha.murtin@icloud.com>
2022-12-19 00:43:56 +0100

    ✨Cleaned up landing page

--- src/App.fs
+++ src/App.fs
@@ -13,19 +13,31 @@ type State =
     struct
         val mutable hue: float
     end
-        
 
 let mutable state = State()
 
+let setupWindow () =
+    canvas.width  <- window.innerWidth
+    canvas.height <- window.innerHeight
+    
+let onWindowResize event =
+    setupWindow ()
+
 let start' () =
+    window.addEventListener ("resize", onWindowResize)
+
+    setupWindow ()
+
     state.hue <- 0.
 
 let update' dt =
     state.hue <- (state.hue + dt * 10.) % 360.
 
+let padding = 10.
+    
 let draw' dt =
     ctx.fillStyle <- !^ $"hsl({state.hue}, 100%%, 50%%)"
-    ctx.fillRect (0., 0., canvas.width, canvas.height)
+    ctx.fillRect (padding, padding, canvas.width - padding * 2., canvas.height - padding * 2.)
 
 let rec loop time =
     let dt = (time - lastTime) / 1000.
@@ -34,6 +46,7 @@ let rec loop time =
     update' dt
 
     ctx.clearRect (0., 0., canvas.width, canvas.height)
+
     draw' dt
 
     window.requestAnimationFrame(loop)
